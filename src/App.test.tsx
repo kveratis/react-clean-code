b9b189552c2ee47ba4d9f24de7e8a49944306caf
Daniel Petersen <daniel.petersen@rapidcyberdefense.com>
2023-08-19 21:08:18 -0600

    Setup With Testing

---
+++ src/App.test.tsx
@@ -0,0 +1,17 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+describe("Simple working test", () => {
+  it("the title is visible", () => {
+    render(<App />);
+    expect(screen.getByText(/Vite/i)).toBeInTheDocument();
+  });
+
+  it("should increment count on click", async () => {
+    render(<App />);
+    userEvent.click(screen.getByRole("button"));
+    expect(await screen.findByText(/count is 1/i)).toBeInTheDocument();
+  });
+});
